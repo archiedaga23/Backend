6be05400d0633797c50dd01c54905233b5e13ea9
archiedaga <archiedaga23@gmail.com>
2023-05-01 00:22:36 -0100

    Workout controller, service, and DB is added

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 
+const workoutRoutes = require('./v1/routes/workoutRoutes');
+
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
-});
+app.use('/api/v1/workouts', workoutRoutes);
 
 app.listen(PORT, () => {
     console.log(`Your server is now running at port ${PORT}`);
