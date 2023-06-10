5ac3432b90725685dd464eb32a0c42f18e5c29bc
archiedaga <archiedaga23@gmail.com>
2023-06-10 14:44:37 -0100

    MongoDB Compass connection

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
+const url = 'mongodb://127.0.0.1:27017/training-application';
 const workoutRoute = require('./v1/routes/workoutRoute');
 
 const PORT = 3000;
+const db = mongoose.connection;
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+db.once('open', _ => {
+    console.log(`Database connected: ${url}`);
+})
+
+db.on('error', err => {
+    console.error(`connecttion error ${err}`)
+})
 
 app.use('/api/v1/workouts', workoutRoute);
 
